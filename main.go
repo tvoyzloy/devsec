fb91b3747a0f49b9b56d093f0fd1ea86f48d352a
Иван Тихомиров <taylongpapi@MacBook-Pro-Ivan.local>
2025-11-27 16:59:36 +0300

    solution

--- main.go
+++ main.go
@@ -9,130 +9,106 @@ import (
 	"time"
 )
 
+const statsURL = "http://srv.msk01.gigacorp.local/_stats"
+
 func main() {
 	errorCount := 0
 
 	for {
-		resp, err := http.Get("http://srv.msk01.gigacorp.local/_stats")
+		resp, err := http.Get(statsURL)
 		if err != nil {
-			errorCount++
-			if errorCount >= 3 {
-				fmt.Println("Unable to fetch server statistic")
-				return
-			}
-			time.Sleep(time.Second)
+			errorCount = handleError(errorCount)
 			continue
 		}
 
-		if resp.StatusCode != 200 {
-			errorCount++
-			if errorCount >= 3 {
-				fmt.Println("Unable to fetch server statistic")
-				return
-			}
-			time.Sleep(time.Second)
+		if resp.StatusCode != http.StatusOK {
+			resp.Body.Close()
+			errorCount = handleError(errorCount)
 			continue
 		}
 
 		body, err := io.ReadAll(resp.Body)
 		resp.Body.Close()
 		if err != nil {
-			errorCount++
-			if errorCount >= 3 {
-				fmt.Println("Unable to fetch server statistic")
-				return
-			}
-			time.Sleep(time.Second)
+			errorCount = handleError(errorCount)
 			continue
 		}
 
-		parts := strings.Split(strings.TrimSpace(string(body)), ",")
-		if len(parts) != 7 {
-			errorCount++
-			if errorCount >= 3 {
-				fmt.Println("Unable to fetch server statistic")
-				return
-			}
-			time.Sleep(time.Second)
-			continue
-		}
-
-		vals := make([]int64, 7)
-		ok := true
-		for i := range parts {
-			v, err := strconv.ParseInt(parts[i], 10, 64)
-			if err != nil {
-				ok = false
-				break
-			}
-			vals[i] = v
-		}
-
+		values, ok := parseStats(body)
 		if !ok {
-			errorCount++
-			if errorCount >= 3 {
-				fmt.Println("Unable to fetch server statistic")
-				return
-			}
-			time.Sleep(time.Second)
+			errorCount = handleError(errorCount)
 			continue
 		}
 
-		// Сброс ошибки при успешном получении корректных данных
 		errorCount = 0
-
-		// --------------------------------------------
-		// 1) Load Average
-		// --------------------------------------------
-		loadAvg := vals[0]
-		if loadAvg > 30 {
-			fmt.Printf("Load Average is too high: %d\n", loadAvg)
-		}
-
-		// --------------------------------------------
-		// 2) Memory usage
-		// --------------------------------------------
-		totalMem := vals[1]
-		usedMem := vals[2]
-
-		if totalMem > 0 {
-			percent := usedMem * 100 / totalMem
-			if percent > 80 {
-				fmt.Printf("Memory usage too high: %d%%\n", percent)
-			}
-		}
-
-		// --------------------------------------------
-		// 3) Disk free space
-		// --------------------------------------------
-		totalDisk := vals[3]
-		usedDisk := vals[4]
-
-		if totalDisk > 0 {
-			freeBytes := totalDisk - usedDisk
-			percentUsed := usedDisk * 100 / totalDisk
-			if percentUsed > 90 {
-				freeMb := freeBytes / (1024 * 1024)
-				fmt.Printf("Free disk space is too low: %d Mb left\n", freeMb)
-			}
-		}
-
-		// --------------------------------------------
-		// 4) Network bandwidth
-		// --------------------------------------------
-		totalNet := vals[5]
-		usedNet := vals[6]
-
-		if totalNet > 0 {
-			// Условие превышения: > 90%
-			if usedNet > totalNet*9/10 {
-				// ВАЖНО: тесты ожидают расчёт в Мбит/с через деление на 125000
-				freeMbit := (totalNet - usedNet) / 125000
-				fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", freeMbit)
-			}
-		}
-
-		// Периодичность выполнения
+		report(values)
 		time.Sleep(time.Second)
+	}
+}
+
+func handleError(count int) int {
+	count++
+	if count >= 3 {
+		fmt.Println("Unable to fetch server statistic")
+		// exit once message is printed
+		panic("exit")
+	}
+	time.Sleep(time.Second)
+	return count
+}
+
+func parseStats(data []byte) ([7]int64, bool) {
+	var values [7]int64
+
+	parts := strings.Split(strings.TrimSpace(string(data)), ",")
+	if len(parts) != 7 {
+		return values, false
+	}
+
+	for i, part := range parts {
+		val, err := strconv.ParseInt(strings.TrimSpace(part), 10, 64)
+		if err != nil {
+			return values, false
+		}
+		values[i] = val
+	}
+
+	return values, true
+}
+
+func report(vals [7]int64) {
+	loadAvg := vals[0]
+	totalMem := vals[1]
+	usedMem := vals[2]
+	totalDisk := vals[3]
+	usedDisk := vals[4]
+	totalNet := vals[5]
+	usedNet := vals[6]
+
+	if loadAvg > 30 {
+		fmt.Printf("Load Average is too high: %d\n", loadAvg)
+	}
+
+	if totalMem > 0 {
+		memPercent := usedMem * 100 / totalMem
+		if memPercent > 80 {
+			fmt.Printf("Memory usage too high: %d%%\n", memPercent)
+		}
+	}
+
+	if totalDisk > 0 {
+		diskPercent := usedDisk * 100 / totalDisk
+		if diskPercent > 90 {
+			freeMb := (totalDisk - usedDisk) / (1024 * 1024)
+			fmt.Printf("Free disk space is too low: %d Mb left\n", freeMb)
+		}
+	}
+
+	if totalNet > 0 {
+		netPercent := usedNet * 100 / totalNet
+		if netPercent > 90 {
+			freeMbit := (totalNet - usedNet) * 8 / 1024 / 1024
+			fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", freeMbit)
+		}
 	}
 }
